35084dd1e77574914a42eb99b854cb128d890229
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-05-03 13:42:04 +0100

    add base pointer unwinding

--- src/unwinder/frame.rs
+++ src/unwinder/frame.rs
@@ -194,7 +194,13 @@ impl Frame {
 
                 Ok(new_ctx)
             },
-            _ => todo!(),
+            Self::BasePointer(base_pointer) => {
+                let mut new_ctx = ctx.clone();
+                new_ctx[Arch::SP] = *base_pointer;
+                new_ctx[Register(6)] = unsafe { *(*base_pointer as *const usize) };
+                new_ctx[Arch::RA] = unsafe { *(*base_pointer as *const usize).offset(1) };
+                Ok(new_ctx)
+            }
         }
     }
 
